50260b0cd22d542fa7fcb8ac0c3bd21b19e61141
RohitRG-123 <rohit.gadakari1107@gmail.com>
2023-09-25 16:12:21 +0530

    initial commit

---
+++ src/main/java/com/ashokit/user/UserDemo.java
@@ -0,0 +1,9 @@
+package com.ashokit.user;
+
+public class UserDemo {
+	public static void main(String[] args) {
+		System.out.println("Hello Bhaiyya");
+		System.out.println("Welcome to my small world!");
+	}
+
+}
